a0211f22de77de0b4fa3670c10b61ce8b68fc4bd
owlwalks <dangkhoa.129@gmail.com>
2018-12-04 18:49:02 +1000

    use net.Conn interface for tls

--- conn.go
+++ conn.go
@@ -2,17 +2,18 @@ package unload
 
 import (
 	"net"
+	"time"
 )
 
 type tcpConn struct {
-	*net.TCPConn
+	rwc  net.Conn
 	uri  []byte
 	host []byte
 	busy bool
 }
 
 func (c *tcpConn) Read(b []byte) (int, error) {
-	n, err := c.TCPConn.Read(b)
+	n, err := c.rwc.Read(b)
 	c.busy = true
 	if err != nil {
 		c.busy = false
@@ -21,7 +22,7 @@ func (c *tcpConn) Read(b []byte) (int, error) {
 }
 
 func (c *tcpConn) Write(b []byte) (int, error) {
-	n, err := c.TCPConn.Write(b)
+	n, err := c.rwc.Write(b)
 	c.busy = true
 	if err != nil {
 		c.busy = false
@@ -29,6 +30,32 @@ func (c *tcpConn) Write(b []byte) (int, error) {
 	return n, err
 }
 
-func newConn(c *net.TCPConn) *tcpConn {
+func (c *tcpConn) Close() error {
+	return c.rwc.Close()
+}
+
+func (c *tcpConn) SetReadDeadline(t time.Time) error {
+	return c.rwc.SetReadDeadline(t)
+}
+
+func (c *tcpConn) SetKeepAlive(keepalive bool) error {
+	if conn, ok := c.rwc.(*net.TCPConn); ok {
+		return conn.SetKeepAlive(keepalive)
+	}
+	return nil
+}
+
+func (c *tcpConn) SetKeepAlivePeriod(d time.Duration) error {
+	if conn, ok := c.rwc.(*net.TCPConn); ok {
+		return conn.SetKeepAlivePeriod(d)
+	}
+	return nil
+}
+
+func (c *tcpConn) RemoteAddr() net.Addr {
+	return c.rwc.RemoteAddr()
+}
+
+func newConn(c net.Conn) *tcpConn {
 	return &tcpConn{c, nil, nil, false}
 }
